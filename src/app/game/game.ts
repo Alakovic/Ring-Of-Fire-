47d7179aac8745aaedf878061d57749878370052
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2025-10-28 18:54:47 +0100

    Show correct card

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -1,20 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { GameModel } from './../../models/game';
 
 @Component({
   selector: 'app-game',
-  standalone:true,
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './game.html',
   styleUrls: ['./game.scss'],
 })
 
 export class Game {
-  cards:number[] = [0,1,2,3];
-  pickCardAnimation:boolean = false;
+  cards: number[] = [0, 1, 2, 3];
+  pickCardAnimation: boolean = false;
+  currentCard: string = '';
+  game?: GameModel;
 
-  takeCard(){
-    this.pickCardAnimation = true;
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new GameModel();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game?.stack.pop() || '';
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
 }
